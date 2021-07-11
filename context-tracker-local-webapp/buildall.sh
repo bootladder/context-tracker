5285a19ca5345e4d131558407166f04b335bdc11
Steve Anderson <steve@bootladder.com>
2021-07-11 08:17:49 -0400

    buildall.sh

---
+++ context-tracker-local-webapp/buildall.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+pushd frontend
+  pushd shell-history
+  echo $(pwd)
+  ./runner.sh
+  popd
+
+  pushd context-summary
+  echo $pwd
+  ./runner.sh
+  popd
+
+  pushd context-viewer
+  echo $pwd
+  ./runner.sh
+  popd
+popd
+
+pushd backend
+  echo $(pwd)
+  go build
+popd
