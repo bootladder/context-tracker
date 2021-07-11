3cfc15a32a02134c5aa99dbefaf8f0f0e089218f
Steve Anderson <steve@bootladder.com>
2021-07-11 05:01:00 -0400

    python was easier than i thought...

--- ash_collector/ash_collector.py
+++ ash_collector/ash_collector.py
@@ -1,1 +1,43 @@
-print("hello")
+#!/usr/bin/python
+
+# this shall be a one-shot command to request a JSON of shell history
+# the JSON will contain an array of history entries
+
+# Parameters may be such as
+# - number of entries
+# - search string
+# - date range
+# - from cwd
+
+# By default it will be the last 100 (10 for now, testing) shell commands
+
+import sqlite3
+import json
+
+conn = sqlite3.connect('/home/steve/.ash/history.db')
+
+# want to look at schema? for testing only
+if False:
+  cursor = conn.execute("SELECT sql from sqlite_master where name='commands'")
+  for row in cursor:
+    for poop in row:
+      print(poop)
+    print('wat')
+
+
+# Get the Rows
+cursor = conn.execute("SELECT * from commands ORDER BY id desc limit 4")
+outputrows = []
+for row in cursor:
+  thisoutputrow = dict()
+  thisoutputrow['cwd']         = row[6]
+  thisoutputrow['startime']    = row[8]
+  thisoutputrow['command']     = row[13]
+  outputrows.append(thisoutputrow)
+
+conn.close()
+
+
+print(json.dumps(outputrows))
+#for outputrow in outputrows:
+#  print(outputrow)
