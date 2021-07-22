1d872e404fed001b4e090a3c0b39ce3e5a6d51b6
Steve Anderson <steve@bootladder.com>
2021-07-22 08:36:34 -0400

    basic source2filename storage for json in data-receiver.py

--- collectord/data-receiver/data-receiver.py
+++ collectord/data-receiver/data-receiver.py
@@ -1,26 +1,55 @@
 #!/usr/bin/python
+
+# note  /var/lib/collectord has to be acceisible
+
 import time
+import json
 import zmq
 
-context = zmq.Context()
-socket = context.socket(zmq.REP)
-socket.bind("tcp://*:5555")
 
-while True:
-    #  Wait for next request from client
-    message = socket.recv()
-    print("Received msg.")
+def main():
+  context = zmq.Context()
+  socket = context.socket(zmq.REP)
+  socket.bind("tcp://*:5555")
 
-    # check for valid json
+  while True:
+      #  Wait for next request from client
+      message = socket.recv()
+      print("Received msg.")
 
-    # look for magic fields
+      try:
+        # check for valid json
+        collection_object = json.loads(message)
 
-    # find the appropriate .json file
+        # look for magic fields
+        source = collection_object['source']
+        print("SOURCE!!! ", source)
 
-    # append the entire message to the file
+        # find the appropriate .json file
+        filename = source2filename(source)
+        print("filename is " + filename)
 
-    #  Do some 'work'
-    time.sleep(1)
+        # append the entire message to the file
+        print(message)
+        with open(filename, 'a+') as f:
+          f.write(message)
 
-    #  Send reply back to client
-    socket.send(b"OK")
+        #  Do some 'work'
+        time.sleep(1)
+
+        #  Send reply back to client
+        socket.send(b"OK")
+
+      except Exception as e:
+        print(e)
+        print("Pbad")
+        socket.send(b"BAD")
+
+
+def source2filename(source):
+  # check for bad characters
+  return "/var/lib/collectord/" + source.replace('_','').replace('.','') + ".json"
+
+
+if __name__ == "__main__":
+  main()
