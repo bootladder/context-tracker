be24fd234cd4c5678c8a37f3ba9657b2efb9d46b
Steve Anderson <steve@bootladder.com>
2021-07-28 21:33:36 -0400

    inotify_collector watches /home/steve/, add filtering filenames

--- collectors/inotify-collector/inotify_collector_oneshot_stdin.py
+++ collectors/inotify-collector/inotify_collector_oneshot_stdin.py
@@ -17,17 +17,32 @@ def process_line_and_send_to_queue(line):
   collection_object['data']['filename'] = values[1]
   collection_object['data']['operation'] = values[2]
 
-  jsondump = json.dumps(collection_object)
+  jsondump = json.dumps(collection_object, indent=2)
   print(jsondump)
 
   collectord_messagequeue.send_message(socket, jsondump)
 
+
+# start with obvious ones, swap, locks
+def i_should_filter(line):
+  values = line.split(',')
+  if values[1].endswith('.swp') \
+  or values[1].endswith('.lock') \
+  or values[1].endswith('.d') \
+  or values[1].endswith('.o') \
+  or ('/.mozilla/firefox' in values[1] and '.default-release' in values[1]):
+    print("\nFiltered Filename: " + values[1])
+    return True
+
+  return False
 
 
 socket = collectord_messagequeue.start_client()
 
 # This is csv. read lines
 for line in sys.stdin:
+  if i_should_filter(line):
+    continue
   process_line_and_send_to_queue(line)
 
 
