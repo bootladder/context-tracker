e60aca249c3903372ca418de40ae9d9b95b3d36a
steve <steve@bootladder.com>
2021-10-18 17:15:12 -0400

    cleanup/comment firefox-collector-daemon.py

--- collectors/collector_utils/collectord_messagequeue.py
+++ collectors/collector_utils/collectord_messagequeue.py
@@ -15,3 +15,14 @@ def send_message(socket, msg):
   socket.send_string(msg)
   message = socket.recv()
   print("receivved from socket : ", message)
+
+# TEMPORARILY DUMPING THIS HERE, THIS NEEDS TO BE A SEPARATE UTIL SCRIPT
+#
+# want to look at schema? for testing only
+#if False:
+#  cursor = conn.execute("SELECT sql from sqlite_master")
+#  for row in cursor:
+#    for poop in row:
+#      print(poop)
+#    print('wat')
+#
--- collectors/firefox-collector/firefox-collector-daemon.py
+++ collectors/firefox-collector/firefox-collector-daemon.py
@@ -7,50 +7,60 @@ import sys
 from shutil import copyfile
 import time
 
-import collectord_messagequeue
+def get_exact_path_to_firefox_sqlite():
+  path_to_firefox_history_db = "/home/*/.mozilla/firefox/*default*/places.sqlite"
 
+  globresult = glob.glob(path_to_firefox_history_db)
+  if len(globresult) != 1:
+    print("wtf too many globs")
+    sys.exit(1)
+
+  #print("database is at " , globresult[0])
+  dbfilename = globresult[0]
+
+
+
+# Setup collectord messagequeue to send JSON to
+import collectord_messagequeue
 socket = collectord_messagequeue.start_client()
 
-path_to_firefox_history_db = "/home/*/.mozilla/firefox/*default*/places.sqlite"
-
-globresult = glob.glob(path_to_firefox_history_db)
-if len(globresult) != 1:
-  print("wtf too many globs")
-  sys.exit(1)
-
-#print("database is at " , globresult[0])
-dbfilename = globresult[0]
+dbfilename = get_exact_path_to_firefox_sqlite()
 
 
+# Setup the Collection Object
+#
+# Predefined keyvalues here, will be modified in place by received SQLite events,
+# and then serialized each time to JSON for sending to the collectord message queue
+collection_object = dict()
+collection_object['source'] = "firefox_collector_daemon.py"
+collection_object['version'] = "0.0.1"
+
+
+
+##################################################################
+#
+# SQLite Polling Loop
+#
+#
 
 latest_row_id = 0
-
-# want to look at schema? for testing only
-if False:
-  cursor = conn.execute("SELECT sql from sqlite_master")
-  for row in cursor:
-    for poop in row:
-      print(poop)
-    print('wat')
-
-
 
 while True:
   print("sleep")
   time.sleep(5.0)
-  #print("copy the database because firefox locks it")
+
+  #copy the database because firefox locks it
   shadow_db_location = "/tmp/firefoxshadow.sqlite"
   copyfile(dbfilename, shadow_db_location)
-  conn = sqlite3.connect(shadow_db_location)
 
+  # Connect to shadow db
+  conn = sqlite3.connect(shadow_db_location)
 
   # Get the Rows
   cursor = conn.execute("SELECT * from moz_places where id > %d ORDER BY id desc limit 10" % latest_row_id)
 
-  collection_object = dict()
-  collection_object['source'] = "firefox_collector_daemon.py"
-  collection_object['version'] = "0.0.1"
 
+  # Collect all new rows in SQLite DB
   outputrows = []
   local_max_id = 0
   number_of_rows = 0
@@ -59,9 +69,9 @@ while True:
     number_of_rows = number_of_rows + 1
     thisoutputrow = dict()
     thisoutputrow['last_visit_date']         = row[8]
-    thisoutputrow['url']     = row[1]
-    thisoutputrow['description']     = row[12]
-    thisoutputrow['title']     = row[2]
+    thisoutputrow['url']                     = row[1]
+    thisoutputrow['description']             = row[12]
+    thisoutputrow['title']                   = row[2]
     outputrows.append(thisoutputrow)
 
     # track highest id
@@ -71,21 +81,27 @@ while True:
 
     print("got row id ", row[0])
 
-  # this is how we check for new rows coming in
+  # If no new rows, stop here
   if number_of_rows == 0:
     print("no new rows")
     continue
-  #
-  # prepare object for sending
+
+
+  # Prepare Collection Object for sending N new rows to collector message queue
   collection_object['rows'] = outputrows
-  #
-  # update max id
+
+  # Update max id (prepping for next query at top of loop)
   latest_row_id = local_max_id
   print("new max id is ", latest_row_id)
-  # write the maxid to a file sometimes
 
+  # Write the maxid to a file sometimes (TODO)
+
+  # Serialize collection object to JSON
   jsondump = json.dumps(collection_object, indent=2)
   print(jsondump)
+
+  # Send JSON to Message Queue
   collectord_messagequeue.send_message(socket, jsondump)
 
-  conn.close() # close every lop
+  # Close connection to shadow db every loop
+  conn.close()
