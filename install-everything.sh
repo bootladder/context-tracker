50eb6195c7e30b09a3b34d590e8cd4b83c774fda
Steve Anderson <steve@bootladder.com>
2021-12-28 20:46:23 -0500

    skeleton install-everything.sh

---
+++ install-everything.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# build the local webapp
+
+
+# install all systemd services
+
+# for each collector adn the data receiver and local webapp
+# copy the daemon to /usr/local/bin
+# copy systemd.unit scriptt to /etc
+# stop enable start systemd service
+
+
+
+
+
+
